f4cdf9f25ff14db3036f9558f6fe733d7cf09f9e
AB MisHkat <mishkat606@gmail.com>
2025-01-23 21:58:14 +0600

    eslint issue solved

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -12,9 +12,15 @@ const Hero = () => {
             <div className="w-full px-4">
               <div className="mx-auto max-w-[800px] text-center">
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  Welcome to MT-07 <br/> Innovating Your Future
+                  Welcome to MT-07 <br /> Innovating Your Future
                 </h1>
-                <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">Here, we’re passionate about building cutting-edge solutions that drive growth, innovation, and success. Whether you're a business leader, an entrepreneur, or a tech enthusiast, our platform is here to empower you with the tools and knowledge to stay ahead in the digital era.</p>
+                <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
+                  Here, we are passionate about building cutting-edge solutions
+                  that drive growth, innovation, and success. Whether you're a
+                  business leader, an entrepreneur, or a tech enthusiast, our
+                  platform is here to empower you with the tools and knowledge
+                  to stay ahead in the digital era.
+                </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
                     href="#"
